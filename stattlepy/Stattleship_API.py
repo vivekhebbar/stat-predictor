1f220677c4f26c8754df09063be3d7c3612be6a6
vivekhebbar <vhebbar13@gmail.com>
2016-10-03 17:40:03 -0700

    Creates roster table inside nfl.db - roster has a record for every player on every team. Data requested from Stattleship and then inserted into SQLite3 table

---
+++ stattlepy/Stattleship_API.py
@@ -0,0 +1,154 @@
+#!/usr/bin/env python
+
+### Version 0.0.1
+
+#Install: 
+#git clone https://github.com/stattleship/stattleship-python.git
+#cd /PATH/TO/DIRECTORY/
+#sudo python setup.py install
+
+#Usage:
+#New_query = Stattleship()
+#Token = New_query.set_token('YOUR_TOKEN')
+#Output = New_query.ss_get_results(sport='basketball',league='nba',ep='game_logs',player_id='nba-stephen-curry')
+
+import requests
+import json
+import math
+import time
+import re
+import platform
+import stattlepy
+
+### Main class that all Stattleship functions will be a part of
+class Stattleship(object):
+    
+        
+        
+        # function to set the token
+        def set_token(self, pro_token):        
+                if pro_token is None or not isinstance(pro_token,basestring):
+                         warnings.warn('Stattleship API token must be provided in order to access the Stattleship API.')
+                else:
+                        global token
+                        token = pro_token
+                
+        ### function to get the results for them Stattleship API
+        def ss_get_results(self,**kwargs):
+
+                # initial defaults for all variables
+                sport = "hockey"
+                league = "nhl"
+                ep = "teams"
+                query = list()
+                version = 1
+                walk = False
+                page = 1
+                verbose = True
+                place = None
+                stat_type = None
+                param = {}
+                
+                # loop through inputs and 
+                for key, value in kwargs.iteritems():
+                        if str(key) == 'sport':
+                                sport = value
+                        elif str(key) == 'league':
+                                league = value
+                        elif str(key) == 'ep':
+                                ep = value
+                        elif str(key) == 'version':
+                                version = value
+                        elif str(key) == 'walk':
+                                walk = value
+                        elif str(key) == 'page':
+                                page = value 
+                        elif str(key) == 'verbose':
+                                verbose = value
+                        elif str(key) == 'stat_type':
+                             param['type'] = value
+                        else:
+                                param[key] = value
+                
+                ### initial verbose to indicate request occurring
+                # if verbose:
+                    # print'Making Initial API Request'
+                    
+                ### initial query       
+                tmp, return_header = self.query_api(sport, league, ep, param, version, walk, page, verbose, token)
+               
+                ### make response list
+                response = list()
+               
+                ### set the original first parsed 
+                response.append(tmp)
+                
+                ### walk function using REGEX to idenify the next link in the header to pull next request 
+                if(walk):
+                    while 'link' in return_header:
+                        
+                        ### Next link to request from the API
+                        next_link = re.findall( 'rel="last", <(.*?)>; rel="next"', return_header['link'], re.MULTILINE)
+                        
+                        ### Use try and except to see if the next link was found within the header
+                        try:
+                            if verbose:
+                                print 'Next link sent to API:'
+                                print next_link[0]
+
+                            headers = {
+                            'Authorization': token,
+                            'Accept':'application/vnd.stattleship.com; version=%s' %version,
+                            'Content-Type':'application/json',
+                            'User-Agent':'Stattleship Python/{} ({})'.format(stattlepy.__version__,platform.platform())
+                            }
+                            
+                            print headers
+
+                            res = requests.get(next_link[0], headers = headers)
+
+                            content = json.loads(res.content)
+
+                            response.append(content)
+
+                            return_header = res.headers
+
+                            ### delay in making call
+                            time.sleep(0.1)
+                            
+                        except IndexError:
+                            break
+                        
+                # print 'Stattleship API request complete'                
+                return(response)
+            
+        def query_api(self, sport, league, ep, param, version, walk, page, verbose, token):
+        
+                ### make sure that the sport, league and ep are all lower case
+                sport = sport.lower()
+                league = league.lower()
+                ep = ep.lower()        
+                
+                ### base url to make the request from
+                url = 'https://api.stattleship.com/{}/{}/{}'.format(sport, league, ep)
+                
+                ### depends on page being requested
+                if page >= 1:
+                        param['page'] = page
+                
+                headers = {
+                        'Authorization': token,
+                        'Accept':'application/vnd.stattleship.com; version=%s' %version,
+                        'Content-Type':'application/json',
+                        'User-Agent':'Stattleship Python/{} ({})'.format(stattlepy.__version__,platform.platform())
+                         }
+                
+                res = requests.get(url,params=param, headers = headers)
+                
+                # if verbose:
+                #     print res
+                #     print res.url
+                
+                content = json.loads(res.content)
+               
+                return(content, res.headers)
--- stattleship_football.py
+++ stattleship_football.py
@@ -1,63 +1,60 @@
 from stattlepy import Stattleship
+import sqlite3
 
 
+# Stattleship-SPECIFIC CONSTANTS
 accessToken = "d49525fb16260a10a902f32b33aaa172"
-
-def getAllPositions():
-	New_query = Stattleship()
-	Token = New_query.set_token(accessToken)
-	qb_list = []
-	rb_list = []
-	wr_list = []
-	te_list = []
-	for i in range(250):
-		Output = New_query.ss_get_results(sport='football',league='nfl',ep='players',page=str(i))
-		qbs = filter(lambda row: row['position_name']=='Quarterback', Output[0]['players'])
-		rbs = filter(lambda row: row['position_name']=='Running Back', Output[0]['players'])
-		wrs = filter(lambda row: row['position_name']=='Wide Receiver', Output[0]['players'])
-		tes = filter(lambda row: row['position_name']=='Tight End', Output[0]['players'])
-		qb_names = map(lambda row: row['name'], qbs)
-		rb_names = map(lambda row: row['name'], rbs)
-		wr_names = map(lambda row: row['name'], wrs)
-		te_names = map(lambda row: row['name'], tes)
-		qb_list += qb_names
-		rb_list += rb_names
-		wr_list += wr_names
-		te_list += te_names
-	print(rb_list)
-	print(wr_list)
-	print(te_list)
-	return qb_list, rb_list, wr_list, te_list
-
-def getAllPlayers(position_name):
-	New_query = Stattleship()
-	pos_list = []
-	Token = New_query.set_token(accessToken)
-	for i in range(220):
-		Output = New_query.ss_get_results(sport='football',league='nfl',ep='players',page=str(i))
-		pos = filter(lambda row: row['position_name']==position_name, Output[0]['players'])
-		pos_names = map(lambda row: row['name'], qbs)
-		pos_list += pos_names
-	print(pos_list)
-	return pos_list
+# NFL-SPECIFIC CONSTANTS
+legitimate_positions = ['QB', 'RB', 'WR', 'TE']
+current_season = "nfl-2016-2017"
 
 
-def computePoints(query, player_id):
-	return None
-
-
-qb_list, rb_list, wr_list, te_list = getAllPositions()
-
-with open('qb_list.text','w') as f:
-	for qb in qb_list:
-		f.write(qb + "\n")
-with open('rb_list.txt', 'w') as f:
-	for rb in rb_list:
-		f.write(rb + "\n")
-with open('wr_list.txt', 'w') as f:
-	for wr in wr_list:
-		f.write(wr + "\n")
-with open('te_list.txt', 'w') as f:
-	for te in te_list:
-		f.write(te + "\n")
-
+# This function builds the roster table, which contains record entries
+# for all current players of all teams in the form:
+# (first name, last name, team slug, position, player slug, player id)
+def populateRoster():
+	new_query = Stattleship()
+	token = new_query.set_token(accessToken)
+	# Query stattleship for NFL teams
+	teams = new_query.ss_get_results(sport='football',league='nfl',ep='teams')
+	# Construct list of all  team_slugs
+	team_slugs = [item['slug'] for item in teams[0]['teams']]
+	# Open DB connection and create roster table
+	conn = sqlite3.connect('nfl.db')
+	c = conn.cursor()
+	c.execute('''CREATE TABLE IF NOT EXISTS roster (first text, last text,
+		team text, position text, slug text, id text PRIMARY KEY)''')
+	conn.commit()
+	# Loop through each team in team_slugs
+	for team in team_slugs:
+		# player_records will contain all of the team's player records to be inserted
+		# into roster at end of for loop
+		player_records = []
+		# Want to look at all valid pages of players for a given team
+		for index in range(1, 30):
+			# Query stattleship for a page of team's roster
+			page = new_query.ss_get_results(sport='football',league='nfl', \
+				ep='players', team_id=team, season_id=current_season, page=str(index))
+			page_of_players = page[0]['players']
+			# If there are no more players to consider, move to next team
+			if not page_of_players:
+				break
+			# Loop through each player on page
+			for player in page_of_players:
+				# If player should be considered, add his record to be inserted
+				if player['position_abbreviation'] in legitimate_positions:
+					record = (player['first_name'], player['last_name'], \
+						team, player['position_abbreviation'], player['slug'], \
+						player['id'])
+					player_records += [record]
+		# Insert team's player records, commit
+		sql_stmt = "INSERT OR REPLACE INTO roster VALUES (?, ?, ?, ?, ?, ?)"
+		c.executemany(sql_stmt, player_records)
+		conn.commit()
+		print "Done with team " + team + ". "
+	# Compute the number of records added within for loops
+	c.execute("SELECT COUNT(*) FROM roster")
+	num_records = c.fetchone()[0]
+	print "There are " + str(num_records) + " records in the roster table."
+	# Upon looping through all teams, close DBconnection
+	conn.close()
